ebe4e0f54d435d700d4cf78508710d4fdff41b94
Shivam <53144060+black-sheepp@users.noreply.github.com>
2023-11-05 04:56:47 +0530

    Update README.md

--- todoBackend/App.js
+++ todoBackend/App.js
@@ -1,23 +1,23 @@
-// Import necessary modules
-const express = require("express"); // Import the Express framework
-const cors = require("cors"); // Import CORS middleware for cross-origin requests
-const App = express(); // Create an Express application instance
+
+const express = require("express"); 
+const cors = require("cors"); 
+const App = express(); 
 const env = require("dotenv").config(); // Load environment variables from.env file
 const port = 8080;
-const db = require("./config/mongoose"); // Connect to the database using Mongoose
+const db = require("./config/mongoose"); 
 const bodyParser = require("body-parser"); 
 const cookieParser = require("cookie-parser"); 
 
 // Middleware setup
-App.use(express.json()); // Parse JSON request bodies
+App.use(express.json()); 
 App.use(bodyParser.json());
 
-App.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies with extended mode
-App.use(cookieParser()); // Parse cookies in incoming requests
-App.use(cors()); // Enable CORS for cross-origin requests
+App.use(bodyParser.urlencoded({ extended: true })); 
+App.use(cookieParser()); 
+App.use(cors()); 
 
 // Routing setup
-App.use("/", require("./routes/index")); // Uses the index route for the main application routing
+App.use("/", require("./routes/index")); 
 
 // Starts the server
 App.listen(port, () => console.log("TalkingDocs listening on port: " + port)); // Listen on the specified port and log a message 
